e5da39a076885d6a2cd7e1704543b0ec22dcef79
Salmakhl <147110378+Salmakhl@users.noreply.github.com>
2023-10-19 21:58:14 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,3 @@
+#ecrire un algorithme qui demande un nombre entier a l’utilisateur, puis il calcule et affichele carre de ce nombre
+n=int(input("tapez un nombre entier: "))
+print("le carré de ce nombre est:", n*n)
